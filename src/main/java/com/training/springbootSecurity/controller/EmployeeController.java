ba72052c5d15a04edf218c188202cba9fd6dd22e
Saxena, Ashwani <ashwani_saxena@optum.com>
2023-10-24 12:22:45 +0530

    update

--- src/main/java/com/training/springbootSecurity/controller/EmployeeController.java
+++ src/main/java/com/training/springbootSecurity/controller/EmployeeController.java
@@ -1,7 +1,12 @@
 package com.training.springbootSecurity.controller;
 
+import java.security.Principal;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +23,15 @@ public class EmployeeController {
 	@GetMapping("/")
 	public List<Employee> getRoot(){
 		return List.of();
+	}
+	
+	@GetMapping("/pricipal")
+	public Principal getPrincipal(Principal principal){
+		return principal;
+	}
+	
+	@GetMapping("/user")
+	public Map<String, Object>  getUser(@AuthenticationPrincipal OAuth2User principal){
+		return Collections.singletonMap("name", principal.getAttribute("login"));
 	}
 }
